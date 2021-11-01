b12efc99ec4cc7099f334bde12655dc9a318e56a
Fede Conte Grand <fede.contegrand@gmail.com>
2021-11-01 17:14:03 -0300

    qe

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,8 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${postgresql-convex-21529 ||process.env.DATABASE_URL|| DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
