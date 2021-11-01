c81eefd8179211452f1c97e47a5924099db3a4e8
Fede Conte Grand <fede.contegrand@gmail.com>
2021-11-01 17:41:58 -0300

    saso

--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 
-const sequelize = new Sequelize("postgres://qrnjhhvbojkreg:af881c035ce77a95e33bb486f52234425d8f68cfd5cac88f710c9380dccc2b11@ec2-3-228-86-183.compute-1.amazonaws.com:5432/d13p4ak6sihg6m" || `postgres://${DB_USER}:${DATABASE_URL|| DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize("postgres://qrnjhhvbojkreg:af881c035ce77a95e33bb486f52234425d8f68cfd5cac88f710c9380dccc2b11@ec2-3-228-86-183.compute-1.amazonaws.com:5432/d13p4ak6sihg6m", {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
